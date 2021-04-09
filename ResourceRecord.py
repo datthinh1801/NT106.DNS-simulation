1d19308b6b853e65bb4694cbdbab87f21d91a135
datthinh1801 <44528004+datthinh1801@users.noreply.github.com>
2021-04-09 07:49:16 +0700

    Enhance __init__ doc and lowercase all input strings.

--- ResourceRecord.py
+++ ResourceRecord.py
@@ -7,7 +7,7 @@ class ResourceRecord:
             "PTR": 12, "HINFO": 13, "MX": 15, "TXT": 16}
     CLASS = {"IN": 1, "CH": 3, "HS": 4}
 
-    def __init__(self, name: str, rr_type: int, rr_class: int, ttl: int, rdlength: int, rdata: str):
+    def __init__(self, name: str, rr_type: int, rr_class: int, ttl: int, rdata: str):
         """
         Initialze a Resource Record.
 
@@ -16,27 +16,50 @@ class ResourceRecord:
         type        -> 2 bytes of the RR TYPE code
         rr_class    -> 2 bytes of the RR CLASS code
         ttl         -> a 32 bit signed integer specifies the cache interval of this RR. 0 means do not cache
-        rdlength    -> an unsigned 16 bit integer specifies the length in bytes of the RDATA field.
         rdata       -> data of corresponding query type
+
+        type:
+        ._______________________.
+        |   Type    |   Int     |
+        |___________|___________|
+        |   A       |   1       |
+        |   NS      |   2       |
+        |   CNAME   |   5       |
+        |   SOA     |   6       |
+        |   WKS     |   11      |
+        |   PTR     |   12      |
+        |   HINFO   |   13      |
+        |   MX      |   15      |
+        |   TXT     |   16      |
+        |___________|___________|
+
+        class:
+        ._______________________.
+        |   Class   |   Int     |
+        |___________|___________|
+        |   IN      |   1       |
+        |   CH      |   3       |
+        |   HS      |   4       |
+        |___________|___________|
 
         rdata:
         ._______________________________________.
         |    TYPE    |    VALUE                 |
-        .____________|__________________________|
+        |____________|__________________________|
         | A          | A 32 bit Internet address|
-        .____________|__________________________|
+        |____________|__________________________|
         | NS         | A <domain-name> of the   |
         |            | authoritative name server|
         |            | of the given hostname.   |
-        .____________|__________________________|
+        |____________|__________________________|
         | CNAME      | A <domain-name> of       |
         |            | the given hostname.      |
-        .____________|__________________________|
+        |____________|__________________________|
         | HINFO      | CPU:                     |
         |            | A string of CPU type.    |
         |            | OS:                      |
         |            | A string of OS type.     |
-        .____________|__________________________|
+        |____________|__________________________|
         | MX         | PREFERENCE:              |
         |            | A 16 bit int of the      |
         |            | preference value         |
@@ -44,16 +67,19 @@ class ResourceRecord:
         |            | EXCHAGNE:                |
         |            | A <domain-name> to       |
         |            | receive the mail.        |
-        .____________|__________________________|
+        |____________|__________________________|
 
         """
-        if self._validate_(name, rr_type, rr_class, ttl, rdlength, rdata):
+        name = name.lower()
+        rdata = rdata.lower()
+
+        if self._validate_(name, rr_type, rr_class, ttl, rdata):
             self._name = name
             self._type = rr_type
             self._class = rr_class
             self._ttl = ttl
-            self._rdlength = rdlength
-            self._rdata = rdlength
+            self._rdata = rdata
+            self._rdlength = len(self._rdata)
         else:
             self._name = None
             self._type = None
@@ -69,12 +95,12 @@ class ResourceRecord:
                 return False
         return True
 
-    def _validate_(self, name: str, rr_type: int, rr_class: int, ttl: int, rdlength: int, rdata: str) -> bool:
+    def _validate_(self, name: str, rr_type: int, rr_class: int, ttl: int, rdata: str) -> bool:
         """Validate inputs before setting these values for the RR."""
         # check data types
         if not self._check_data_type_(name, rdata, dtype='str'):
             return False
-        elif not self._check_data_type_(rr_type, rr_class, ttl, rdlength, dtype='int'):
+        elif not self._check_data_type_(rr_type, rr_class, ttl, dtype='int'):
             return False
 
         # check if hostname is empty or exceeds the size limit of 255 bytes
@@ -97,9 +123,6 @@ class ResourceRecord:
         # check if ttl < 0
         elif ttl < 0:
             return False
-        # check if rdlength < 0 or rdlength does not match rdata's length
-        elif rdlength < 0 or rdlength != len(rdata):
-            return False
 
         # overall true
         return True
@@ -110,4 +133,4 @@ class ResourceRecord:
         The resulting has 1 line.
         #1 <hostname>;<type>;<class>;<value>
         """
-        return self._name + ";" + self._type + ";" + self._class + ";" + self._rdata
+        return self._name + ";" + str(self._type) + ";" + str(self._class) + ";" + self._rdata
