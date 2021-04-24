ea7aff3b8b89268c3fca2c415c848b31febddbcc
mt2651 <59766640+mt2651@users.noreply.github.com>
2021-04-24 16:05:09 +0700

    Can use cache

--- Cache.py
+++ Cache.py
@@ -1,35 +1,33 @@
 from ResourceRecord import ResourceRecord
+import time
 
 
 class Cache:
     # ResourceRecord
-    """ Cache version 1
-    """
 
-    def __init__(self, record:ResourceRecord = None, ttl=1000):
+    def __init__(self, record:ResourceRecord = None, cache_ttl=1000):
         # Initialize a DNS cache.
         self.data = {}
-        self.ttl = ttl # time to live
-        self.ttd = time.time() + self.ttl # time to die
+        self.cache_ttl = ttl # time to live
+        self.cache_ttd = time.time() + self._cache_ttl # time to die
 
     def reset_cache(self):
-        # Clean the cache if it's time to do so.
-        now = time.time()
-        if self.ttd <= now:
-            keys_to_delete = []
-            for (k, v) in self.data.iteritems():
-                if v.expiration <= now:
-                    keys_to_delete.append(k)
-            for k in keys_to_delete:
-                del self.data[k]
-            now = time.time()
-            self.ttd = now + self.ttl
+        current_time = time.time()
+        if self.cache_ttd >= current_time: # đã đến lúc để dọn dẹp 
+            black_list = [] # hết hạn thì vô đây
+            for (i, rr) in self.data.items():
+                if rr.ttl >= current_time: # ttl of resourcerecord
+                    black_list.append(k) # không thể xoá dictionary khi dùng iteritems
+            for i in black_list:
+                del self.data[i] # nên giờ mới xoá nè
+            current_time = time.time() 
+            self.cache_ttd = current_time + self.cache_ttl
 
     def get(self, key):
         #key = (name: str,rr_type: int, rr_class:int)
         self.reset_cache()
-        v = self.data.get(key)
-        if v is None or v.expiration <= time.time():
+        v = self.data[key]
+        if v is None or v.ttl <= time.time():
             return None
         return v
 
