16d362b5cd05a61e796e331660f156804ec2d1eb
mt2651 <59766640+mt2651@users.noreply.github.com>
2021-05-04 16:15:36 +0700

    fix ttl of RR in cache

--- Cache.py
+++ Cache.py
@@ -5,37 +5,39 @@ import time
 class Cache:
     # ResourceRecord
 
-    def __init__(self, record: ResourceRecord = None, cache_ttl=1000):
+    def __init__(self, record: ResourceRecord = None, time_refresh=300):
         # Initialize a DNS cache.
-        self.data = {}
-        self.cache_ttl = cache_ttl  # time to live
-        self.cache_ttd = time.time() + self.cache_ttl  # time to die
+        self._data = {}
+        self._time_refresh = time_refresh  # time between 2 refresh
+        self._time_refresh_next = time.time() + self._time_refresh  # time next refresh
 
     def refresh_cache(self):
         timestamp = time.time()
-        if self.cache_ttd <= timestamp:  # time refresh
-            black_list = []  # list deletegit
-            for (i, rr) in self.data.items():
-                if rr.ttl >= timestamp:  # ttl of resourcerecord
-                    # không thể xoá dictionary khi dùng iteritems
+        # if "current time" is greater or equal than "time next refresh" mean can be refresh
+        if self._time_refresh_next <= timestamp:
+            black_list = []  # list "key" need delete
+            for (i, rr) in self._data.items():
+                # if time to die of RR > timestamp mean RR is out of date
+                if rr._ttd >= timestamp:
                     black_list.append(i)
+            """
+            cannot delete dictionary when use iteritems
+            =>>> use 2 step to delete
+            """
             for i in black_list:
-                del self.data[i]  # nên giờ mới xoá nè
+                del self._data[i]
             timestamp = time.time()
-            self.cache_ttd = timestamp + self.cache_ttl
+            self._time_refresh_next = timestamp + self._time_refresh
 
     def get(self, key):
-        # key = (name: str,rr_type: int, rr_class:int)
+        # key:(name: str,rr_type: int, rr_class:int)
         self.refresh_cache()
-        rr = self.data.get(key)
-        #rr = self.data[key]
-        if rr is None or rr._ttd <= time.time():
-            return None
-        rr.reset_ttd()
+        rr = self._data.get(key)
+        # rr = self.data[key] not return if data[key] None
         return rr
 
     def put(self, key, record):
-        # key = (name: str,rr_type: int, rr_class:int)
-        # record = ResourceRecord: object
+        # key:(name: str,rr_type: int, rr_class:int)
+        # record:ResourceRecord: object
         self.refresh_cache()
-        self.data[key] = record
+        self._data[key] = record
