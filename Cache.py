a44d8aa30ed2ba2ea9879eea65717308b925ff67
dat._.thinh <44528004+datthinh1801@users.noreply.github.com>
2021-05-05 22:27:17 +0700

    Redefine Cache class to represent a RR in the CacheSystem

--- Cache.py
+++ Cache.py
@@ -1,37 +1,39 @@
 from ResourceRecord import ResourceRecord
-import time
+from copy import deepcopy
+from time import time
 
 
 class Cache:
-    # ResourceRecord
+    def __init__(self, record: ResourceRecord):
+        """
+        Initialize a cache of a given record.
+        ttd of the cache will be computed according to the ttl of the record.
+        """
+        self._record = record
+        self._ttd = int(time()) + record.ttl
 
-    def __init__(self, record: ResourceRecord = None, cache_ttl=1000):
-        # Initialize a DNS cache.
-        self.data = {}
-        self.cache_ttl = cache_ttl  # time to live
-        self.cache_ttd = time.time() + self.cache_ttl  # time to die
+    def get_ttd(self):
+        """Return the ttd of this cache."""
+        return self._ttd
 
-    def refresh_cache(self):
-        timestamp = time.time()
-        if self.cache_ttd <= timestamp:  # time refresh
-            black_list = []  # list delete
-            for (i, rr) in self.data.items():
-                if rr.ttl >= timestamp:  # ttl of resourcerecord
-                    # can not delete dict when use iteritems
-                    black_list.append(i)
-            for i in black_list:
-                del self.data[i]  # delete
-            timestamp = time.time()
-            self.cache_ttd = timestamp + self.cache_ttl
+    def set_ttd(self, ttd: int):
+        """Set the ttd of this cache to the given value."""
+        if ttd > 0:
+            self._ttd = ttd
 
-    def get(self, key):         # key = (name: str,rr_type: int, rr_class:int)
-        self.refresh_cache()
-        rr = self.data.get(key)
-        #rr = self.data[key]
-        if rr is None:# or rr._ttl <= time.time():
-            return None
-        return rr
+    def get_rr(self):
+        """Return a copy of the ResourceRecord of this cache."""
+        return deepcopy(self._record)
 
-    def put(self, key, record):        # record = ResourceRecord: object
-        self.refresh_cache()
-        self.data[key] = record
+    def __eq__(self, other):
+        """
+        Comparison between 2 cached records.
+        If the name, type, and class of both are equal, they are equal;
+        otherwise not equal.
+        """
+        if self.record == other.record:
+            return True
+        return False
+
+    ttd = property(get_ttd, set_ttd)
+    record = property(get_rr)
