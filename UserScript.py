39380244cca62cb97ec79536aa7818e35990a53f
mt2651 <mtxx2651@gmail.com>
2021-05-22 23:01:19 +0700

    Add eception

--- UserScript.py
+++ UserScript.py
@@ -83,9 +83,7 @@ def make_query(args_obj) -> str:
     client_socket.settimeout(1.0)
 
     # Prepare a message for transmission
-    # msg = f"{args_obj.qname};{args_obj.qtype};{args_obj.qclass};{args_obj.protocol}"
-    question = MessageQuestion(args_obj.qname, args_obj.qtype, args_obj.qclass)
-    msg = question.to_string() + ";" + args_obj.protocol
+    msg = f"{args_obj.qname};{args_obj.qtype};{args_obj.qclass};{args_obj.protocol}"
 
     # Send the message to the resolver
     resolver_address = (args_obj.resolver_ip, args_obj.resolver_port)
